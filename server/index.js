97a2d88a071eadf66987c7fe0ca7fdfdcfa4ee04
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-12-08 22:43:08 +0100

    added environment variables

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const cors =require('cors');
+const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,12 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-mongoose.connect("mongodb+srv://negarl:hackday2022@cluster0.65pgd.mongodb.net/minilink?retryWrites=true&w=majority")
+mongoose.connect(
+  `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.65pgd.mongodb.net/minilink?retryWrites=true&w=majority`
+);
 
 //require the route
-app.use('/', require('./routes/urlRoute'))
- 
+app.use('/', require('./routes/urlRoute'));
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
